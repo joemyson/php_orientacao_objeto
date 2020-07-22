84dce7f14abe8aa402d7e27cff73356724f79a7f
joemyson-santos <joemyson1402@gmail.com>
2020-07-22 01:36:10 -0300

    função e construtor

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -5,24 +5,50 @@ class ContaBancaria
 
 {
 
-//propriedade de classe
+//propriedade de classe exemplo $banco,$numeroAgencia
 
 // modificadores de acesso -> public - private - protected
 public $banco;
 public $nomeTitular = 'joemyson';
-public $numeroAgencia;
+public $numeroAgencia = '3467';
 public $numeroConta;
-public $saldo;
+public $saldo = 10000.00;
 
+//craindo construto
+public function _construto()
+{
 
+echo 'ola , sou construtor'
 
 
 
 
 }
 
+// criando função
+public function obterSaldo()
+{
+
+
+return 'ola ,eu sou seu metodo obterSaldo()';
+
+
+
+}
+
+
+
+}
+
 //variavel $conta e uma conta bancaria 
-
+// uso do 'new' para criação de instancias indica que a variavel recebe o tipo
 $conta = new ContaBancaria();
+//$conta recebe todo os atributo da contaBancaria
 
+
+//imprime o nome 
 var_dump($conta -> $nomeTitular);
+
+$conta ->saldo = 0;
+
+echo $conta-> obterSaldo();
